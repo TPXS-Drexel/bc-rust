64772f17d37e5580493b809fc5791fe1026b42d9
Blake Hieu Nguyen <57298440+nvthblake@users.noreply.github.com>
2020-12-04 15:29:22 -0500

    connect and communicate with front end

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,32 @@ use std::sync::{Arc, Mutex};
 use std::sync::atomic::{AtomicBool, Ordering};
 */
 //serde
+// use async_std::fs;
+// use futures::future;
+// use std::path::Path;
+
 extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 use serde::{Deserialize, Serialize};
 use serde_json::{Result, Value};
+
+// HTTP stuff
+extern crate regex;
+#[macro_use]
+extern crate lazy_static;
+
+use actix_web::{error, post, web, App, Error, HttpResponse};
+use regex::Regex;
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 
 //todo: move into inputs
 
@@ -67,10 +86,11 @@ pub struct Input {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Block {
+    id: u64,
     prev_block_hash: Sha256Hash,
     //prev_block_hash_string: String,
     nonce: u64,
-
+    input_data: String,
     data: Vec<u8>,
     //data_string: String,
     hash: Sha256Hash,
@@ -80,10 +100,12 @@ pub struct Block {
 }
 
 impl Block {
-    pub fn new(data: &str, prev_hash: Sha256Hash) -> Self {
+    pub fn new(mut id: u64, data: &str, prev_hash: Sha256Hash) -> Self {
         let mut s = Self {
+            id: id,
             prev_block_hash: prev_hash,
             nonce: 0,
+            input_data: data.to_string(),
             data: data.to_owned().into(),
             hash: hash_without_nonce(data, prev_hash),
             mined: false,
@@ -95,8 +117,8 @@ impl Block {
         s
     }
 
-    pub fn genesis(data: &str) -> Self {
-        Self::new(data, Sha256Hash::default())
+    pub fn genesis(mut id: u64, data: &str) -> Self {
+        Self::new(id, data, Sha256Hash::default())
     }
 
     pub fn mine_block(&mut self, max_nonce: u64, leading_zeros: u64) -> &Self {
@@ -125,21 +147,46 @@ pub struct Blockchain {
 }
 
 impl Blockchain {
-    pub fn new(data: &str) -> Self {
-        let blocks = Block::genesis(data);
+    pub fn new(id: u64, data: &str) -> Self {
+        let blocks = Block::genesis(id, data);
         Self {
             blocks: vec![blocks],
         }
     }
 
-    pub fn add_block(&mut self, data: &str, max_nonce: u64, leading_zeros: u64) {
+    pub fn add_block(&mut self, id: u64, data: &str, max_nonce: u64, leading_zeros: u64) {
         if !self.blocks.last().unwrap().mined {
             let prev_block = &mut self.blocks.pop().unwrap();
             Block::mine_block(prev_block, max_nonce, leading_zeros);
             self.blocks.push(prev_block.to_owned());
         }
-        let block = Block::new(data, self.blocks.last().unwrap().hash);
+        let block = Block::new(id, data, self.blocks.last().unwrap().hash);
         self.blocks.push(block);
+    }
+}
+
+// async fn get_input<P: AsRef<Path>>(path: P) -> Result<Vec<u8>> {
+//     let path = path.as_ref().to_owned();
+//     future::ok(path.to_)
+// }
+
+fn get_request_content(text: &str) -> &str {
+    println!("doing regex");
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"(GET /convert_bc(.*) HTTP)").unwrap();
+    }
+    match RE.captures(text) {
+        Some(caps) => {
+            println!("Found {}", &caps[0]);
+            let end_pos = &caps[0].len() - 5;
+            // let slice = &caps[0][18..end_pos];
+            // println!("slice is {} ", slice);
+            return &caps.get(0).unwrap().as_str()[18..end_pos];
+        }
+        None => {
+            println!("Can't find {}", text);
+            return text;
+        }
     }
 }
 
@@ -194,7 +241,10 @@ fn main() {
 
     //demo of functions above ^^
 
+    // let mut file = File::create("output.json").expect("Could not create file");
+
     // Initiate values when function first start
+    let init_id = 0;
     let init_input = "Null";
     let init_leading_zeros: u64 = 3;
     let init_max_nonce = 1_000_000;
@@ -203,7 +253,7 @@ fn main() {
     //ACTUAL USE OF FUNCTIONS:
     //TAKE FIRST INPUT
     //MATCH STATEMENT FOR LEADING ZEROS CHANGED
-    let mut bc = Blockchain::new(init_input);
+    let mut bc = Blockchain::new(init_id, init_input);
 
     //todo: make threads for server i/o
 
@@ -215,26 +265,40 @@ fn main() {
 
     println!("{:?}", blocka);
 
-    // Receive initial data from front-end
-    let input_json_str = r#"
-        {
-            "content": "Blake",
-            "leading_zeros": 3,
-            "max_nonce": 1000000
-        }"#;
+    // Listen for incoming TCP connections on localhost port 7878
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
-    // Parse string into serde_json::Value
-    let result = serde_json::from_str(input_json_str);
-    if result.is_ok() {
-        let p: Input = result.unwrap();
-        println!("Content is {}", p.content.as_str());
-        // If leading_zeros or max_nonce chanced, rebuild the whole block chain
-        if p.leading_zeros != init_leading_zeros || p.max_nonce != init_max_nonce {
-            // Do something in here
-        } else {
+    // Block forever, handling each request that arrives at this IP address
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+        // Handle connection from stream;
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        // Read and process HTTP request
+        let message = String::from_utf8_lossy(&buffer[..]);
+        println!("Request: {}", message);
+        // Declare 2 expected cases of HTTP request
+        let get = b"GET / HTTP/1.1\r\n";
+        let get_convert = b"GET /convert_bc";
+        // Triage flow based on HTTP request content
+        let (status_line, filename) = 
+        // If HTTP 200 OK
+        if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK\r\n\r\n", "./user_interface/index.html")
+        // If HTTP sends content for new block
+        } else if buffer.starts_with(get_convert) {
+            let mut file = File::create("output.json").expect("Could not create file");
+            println!("Blockchain now");
+            let input_content = get_request_content(&message);
+            println!("{}", input_content);
+            // let result = serde_json::from_str(input_content);
+            // if result.is_ok() {
+            // let p = input_content;
+            println!("Content is {}", input_content);
             Blockchain::add_block(
                 &mut bc,
-                p.content.as_str(),
+                n,
+                input_content,
                 init_max_nonce,
                 init_leading_zeros,
             );
@@ -243,10 +307,35 @@ fn main() {
             n += 1;
             println!("{:?}", mined);
             println!("{:?}", hex::encode(mined.hash));
-            let j = serde_json::to_string(&bc).unwrap();
+            let j = serde_json::to_string(&bc.blocks).unwrap();
             println!("{}", j);
-        }
+            file.write_all(j.as_ref()).expect("Cannot write the file");
+            // }
+            ("HTTP/1.1 200 OK\r\n\r\n", "./user_interface/index.html")
+        // If HTTP sends an unrecognized request
+        } else {
+            (
+                "HTTP/1.1 404 NOT FOUND\r\n\r\n",
+                "./user_interface/index_2.html",
+            )
+        };
+        // Compile and return content
+        let contents = fs::read_to_string(filename).unwrap();
+        let response = format!("{}{}", status_line, contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
+
+    // Receive initial data from front-end
+    // let input_json_str = r#"
+    //     {
+    //         "content": "",
+    //         "leading_zeros": 3,
+    //         "max_nonce": 1000000
+    //     }"#;
+
+    // Parse string into serde_json::Value
+    // hash_input(input_json_str, init_leading_zeros, init_max_nonce, mut blockchain: Blockchain, n);
 
     // for i in 0..4 {
     //     println!("----------{:?}", queue_input.inputs[i].content);
